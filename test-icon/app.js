949f1a0eed046bf88e104d356b0d94d4fd457a86
ALightbolt4G <adham.hossam5020@gmail.com>
2026-06-07 21:22:33 +0300

    chore: ignore test directories and untrack them from git

--- test-icon/app.js
+++
@@ -1,9 +0,0 @@
-import { app } from '@ALightbolt4G/nitron'
-
-app.init({
-  name: "Icon Test",
-  packageId: "com.nitron.icontest",
-  version: "1.0.0",
-  entry: "index.html",
-  icon: "./assets/icon.png"
-})
